366082f3d7f9176bab883d3228eacb7715166c37
David de Andres <ddeandres@juniper.net>
2021-02-12 09:49:30 -0500

    Initial Commit

---
+++ commands2.sh
@@ -0,0 +1,18 @@
+#sudo ip link add link eth1 name eth1.10 type vlan id 10
+sudo ip link set eth1 up
+#sudo ip link set eth1.10 up
+#sudo ip addr add 10.1.1.2/24 dev eth1.10
+sudo ip addr add 10.1.1.2/24 dev eth1
+#sudo ip route add 10.1.0.0/16 via 10.1.1.254 dev eth1.10
+sudo ip route add 10.1.0.0/16 via 10.1.1.254 dev eth1
+
+sudo ip netns add blue
+sudo ip link set eth2 netns blue
+#sudo ip -n blue link add link eth2 name eth2.20 type vlan id 20
+sudo ip -n blue link set eth2 up
+#sudo ip -n blue link set eth2.20 up
+#sudo ip -n blue addr add 10.1.2.2/24 dev eth2.20
+sudo ip -n blue addr add 10.1.2.2/24 dev eth2
+#sudo ip -n blue route add 10.1.0.0/16 via 10.1.2.254 dev eth2.20
+sudo ip -n blue route add 10.1.0.0/16 via 10.1.2.254 dev eth2
+
